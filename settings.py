bcc481d28e3f6b2fd595d67818d68ec7d8a4ef57
gokseninyuksel <Goksenin.yuksel@student.ru.nl>
2022-05-12 13:40:13 +0200

    Changed imports

--- settings.py
+++ settings.py
@@ -2,12 +2,11 @@ from utils.config import Configuration
 import torch
 from torch.utils.tensorboard.writer import SummaryWriter
 
-def init():
-    confjson = Configuration.load_json('conf.json')
-    global writer,sources_names,scaler,counter_train,counter_val
-    scaler = torch.cuda.amp.GradScaler(init_scale=10000,enabled = confjson.mixed_precision)
-    writer = SummaryWriter(confjson.writer_path,flush_secs = 5)
-    sources_names = confjson.sources_names
-    counter_train = 0 
-    counter_val = 0
+confjson = Configuration.load_json('conf.json')
+global writer,sources_names,scaler,counter_train,counter_val
+scaler = torch.cuda.amp.GradScaler(init_scale=10000,enabled = confjson.mixed_precision)
+writer = SummaryWriter(confjson.writer_path,flush_secs = 5)
+sources_names = confjson.sources_names
+counter_train = 0 
+counter_val = 0
 
